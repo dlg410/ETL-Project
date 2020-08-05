f70b0eac7567c954ff99001079a9aef476746f80
Diego <63361814+dlg410@users.noreply.github.com>
2020-08-05 02:34:58 -0400

    ETL

---
+++ ERD/ERD.sql
@@ -0,0 +1,67 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "World Happines Report" (
+    "GDP per Capita" int   NOT NULL,
+    "Life Ladder" int   NOT NULL,
+    "Life Expectancy at Birth" int   NOT NULL,
+    "Freedom to Make Life Choices" int   NOT NULL,
+    "Generosity" int   NOT NULL,
+    "Perceptions of Corruption" int   NOT NULL,
+    "Confidence in National Government" int   NOT NULL
+);
+
+CREATE TABLE "Human Development Index HDI" (
+    "Education Index" int   NOT NULL,
+    "GNI Index" int   NOT NULL,
+    "Life Expectancy Index" int   NOT NULL
+);
+
+CREATE TABLE "Education Index" (
+    "Expected Years of Schooling" int   NOT NULL,
+    "Mean Years of Schooling" int   NOT NULL
+);
+
+CREATE TABLE "Life Expectancy Index" (
+    "Life expectancy at Birth" int   NOT NULL
+);
+
+CREATE TABLE "Gross National Income" (
+    "GDP per Capita" int   NOT NULL,
+    "GDP Total" int   NOT NULL,
+    "GNI per Capita" int   NOT NULL
+);
+
+CREATE TABLE "Inequality-Adjusted HDI" (
+    "Overall Loss in HDI Due to Inequality" int   NOT NULL,
+    "Inequality-Adjusted Education Index" int   NOT NULL,
+    "Inequality-Adjusted Income Index" int   NOT NULL,
+    "Inequality-Adjusted Life Expectancy Index" int   NOT NULL,
+    "Income Inequality, Gini Coefficient" int   NOT NULL,
+    "Inequality in Education" int   NOT NULL,
+    "Inequality in Income" int   NOT NULL,
+    "Inequality in Life Expectancy" int   NOT NULL
+);
+
+ALTER TABLE "World Happines Report" ADD CONSTRAINT "fk_World Happines Report_GDP per Capita_Life Ladder_Life Expectancy at Birth" FOREIGN KEY("GDP per Capita", "Life Ladder", "Life Expectancy at Birth")
+REFERENCES "Human Development Index HDI" ("GNI Index", "Education Index", "Life Expectancy Index");
+
+ALTER TABLE "Education Index" ADD CONSTRAINT "fk_Education Index_Expected Years of Schooling" FOREIGN KEY("Expected Years of Schooling")
+REFERENCES "Human Development Index HDI" ("Education Index");
+
+ALTER TABLE "Education Index" ADD CONSTRAINT "fk_Education Index_Mean Years of Schooling" FOREIGN KEY("Mean Years of Schooling")
+REFERENCES "Human Development Index HDI" ("Education Index");
+
+ALTER TABLE "Life Expectancy Index" ADD CONSTRAINT "fk_Life Expectancy Index_Life expectancy at Birth" FOREIGN KEY("Life expectancy at Birth")
+REFERENCES "Human Development Index HDI" ("Life Expectancy Index");
+
+ALTER TABLE "Gross National Income" ADD CONSTRAINT "fk_Gross National Income_GNI per Capita" FOREIGN KEY("GNI per Capita")
+REFERENCES "Human Development Index HDI" ("GNI Index");
+
+ALTER TABLE "Inequality-Adjusted HDI" ADD CONSTRAINT "fk_Inequality-Adjusted HDI_Inequality-Adjusted Education Index_Inequality-Adjusted Income Index_Inequality-Adjusted Life Expectancy Index" FOREIGN KEY("Inequality-Adjusted Education Index", "Inequality-Adjusted Income Index", "Inequality-Adjusted Life Expectancy Index")
+REFERENCES "Human Development Index HDI" ("Education Index", "GNI Index", "Life Expectancy Index");
+
+ALTER TABLE "Inequality-Adjusted HDI" ADD CONSTRAINT "fk_Inequality-Adjusted HDI_Inequality in Education_Inequality in Income_Inequality in Life Expectancy" FOREIGN KEY("Inequality in Education", "Inequality in Income", "Inequality in Life Expectancy")
+REFERENCES "Human Development Index HDI" ("Education Index", "GNI Index", "Life Expectancy Index");
+
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # ETL-Project
+
+World Happiness Report vs HDI — ETL Project
+By Zachary Frame and Diego Gonzalez
+
+Project Proposal
+Based upon the data compiled by the United Nations Development Program, Kaggle, the World-bank, we want to explore the
+different indicators used to rank countries in both the Human Development Index and the World Happiness Report.
+
+This will be done by web-scrapping both websites in order to find the relevant indicators, and by extracting the CSV data
+from the United Nations website and migrating it to a PostgreSQL Database.
+
+Project Description
+
+Finding Data
+All of the data that we used were from https://undp.org/, https://www.worldbank.org, https://www.kaggle.com  where they
+compiled from various sources that include Eurostat, U.S National Center for Statistics, World Healthcare Organization, etc.. 
+
+Data Cleanup and Analysis
+
+EXTRACTION STEPS 
+				
+Developing a web scraping function using Python, Jupyter Notebooks, Splinter,  that would allows us to collect data from 
+different sources to be used in later stages.
+			
+TRANSFORMATION STEPS 
+
+Our transformation steps we needed to clean the data to be readable, presentable, and easy for us to query in the later
+stages. This was done by:
+
+Developing a cleaning function in Jupiter Notebook that would select the data from 2010 until most recently available. This
+was applied to all datasets that we have.
+
+LOADING STEPS
+
+We established a connection to a local PostgreSQL server in our desktop to store the data
+We have a schema that just makes the tables and we can confirm it through engine.table_names()
+We pushed the Pandas DataFrame to the local PostgreSQL server so we can retrieve and query the data in our Jupyter Notebook
+			
+
+
+ANALYSIS 
+			
+The aim of the analysIs is to determined if there is a correlation between the Human Development Index and The World
+Happiness Report.
+Also if The Human Development Index is dependent on the specific ranking on the that country ranking of the World Happiness
+Report or Viceversa.
+We use data from the Inequality-Adjusted HDI as a reference to see the level of discrepancies.
