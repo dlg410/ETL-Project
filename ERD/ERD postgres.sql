edc333b616a4bfcadb818c45ca1329d7c8873d7d
Diego <63361814+dlg410@users.noreply.github.com>
2020-08-09 13:05:14 -0400

    ERD

---
+++ ERD/ERD postgres.sql
@@ -0,0 +1,49 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "World Happines Report" (
+    "GDP per Capita" int   NOT NULL,
+    "Life Ladder" int   NOT NULL,
+    "Life Expectancy at Birth" int   NOT NULL,
+    "Freedom to Make Life Choices" int   NOT NULL,
+    "Perceptions of Corruption" int   NOT NULL,
+    "Confidence in National Government" int   NOT NULL
+);
+
+CREATE TABLE "Human Development Index HDI" (
+    "Education Index" int   NOT NULL,
+    "GNI Index" int   NOT NULL,
+    "Life Expectancy Index" int   NOT NULL
+);
+
+CREATE TABLE "Education Index" (
+    "Expected Years of Schooling" int   NOT NULL,
+    "Mean Years of Schooling" int   NOT NULL
+);
+
+CREATE TABLE "Life Expectancy Index" (
+    "Life expectancy at Birth" int   NOT NULL
+);
+
+CREATE TABLE "Gross National Income" (
+    "GDP per Capita" int   NOT NULL,
+    "GDP Total" int   NOT NULL,
+    "GNI per Capita" int   NOT NULL
+);
+
+ALTER TABLE "World Happines Report" ADD CONSTRAINT "fk_World Happines Report_GDP per Capita" FOREIGN KEY("GDP per Capita")
+REFERENCES "Gross National Income" ("GNI per Capita");
+
+ALTER TABLE "World Happines Report" ADD CONSTRAINT "fk_World Happines Report_Life Expectancy at Birth" FOREIGN KEY("Life Expectancy at Birth")
+REFERENCES "Life Expectancy Index" ("Life expectancy at Birth");
+
+ALTER TABLE "Human Development Index HDI" ADD CONSTRAINT "fk_Human Development Index HDI_Education Index_Education Index" FOREIGN KEY("Education Index", "Education Index")
+REFERENCES "Education Index" ("Expected Years of Schooling", "Mean Years of Schooling");
+
+ALTER TABLE "Human Development Index HDI" ADD CONSTRAINT "fk_Human Development Index HDI_GNI Index" FOREIGN KEY("GNI Index")
+REFERENCES "Gross National Income" ("GNI per Capita");
+
+ALTER TABLE "Human Development Index HDI" ADD CONSTRAINT "fk_Human Development Index HDI_Life Expectancy Index" FOREIGN KEY("Life Expectancy Index")
+REFERENCES "Life Expectancy Index" ("Life expectancy at Birth");
+
--- ERD/ERD.sql
+++ ERD/ERD.sql
@@ -2,66 +2,63 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "World Happines Report" (
-    "GDP per Capita" int   NOT NULL,
-    "Life Ladder" int   NOT NULL,
-    "Life Expectancy at Birth" int   NOT NULL,
-    "Freedom to Make Life Choices" int   NOT NULL,
-    "Generosity" int   NOT NULL,
-    "Perceptions of Corruption" int   NOT NULL,
-    "Confidence in National Government" int   NOT NULL
-);
+SET XACT_ABORT ON
 
-CREATE TABLE "Human Development Index HDI" (
-    "Education Index" int   NOT NULL,
-    "GNI Index" int   NOT NULL,
-    "Life Expectancy Index" int   NOT NULL
-);
+BEGIN TRANSACTION QUICKDBD
 
-CREATE TABLE "Education Index" (
-    "Expected Years of Schooling" int   NOT NULL,
-    "Mean Years of Schooling" int   NOT NULL
-);
+CREATE TABLE [World Happines Report] (
+    [GDP per Capita] int  NOT NULL ,
+    [Life Ladder] int  NOT NULL ,
+    [Life Expectancy at Birth] int  NOT NULL ,
+    [Freedom to Make Life Choices] int  NOT NULL ,
+    [Perceptions of Corruption] int  NOT NULL ,
+    [Confidence in National Government] int  NOT NULL 
+)
 
-CREATE TABLE "Life Expectancy Index" (
-    "Life expectancy at Birth" int   NOT NULL
-);
+CREATE TABLE [Human Development Index HDI] (
+    [Education Index] int  NOT NULL ,
+    [GNI Index] int  NOT NULL ,
+    [Life Expectancy Index] int  NOT NULL 
+)
 
-CREATE TABLE "Gross National Income" (
-    "GDP per Capita" int   NOT NULL,
-    "GDP Total" int   NOT NULL,
-    "GNI per Capita" int   NOT NULL
-);
+CREATE TABLE [Education Index] (
+    [Expected Years of Schooling] int  NOT NULL ,
+    [Mean Years of Schooling] int  NOT NULL 
+)
 
-CREATE TABLE "Inequality-Adjusted HDI" (
-    "Overall Loss in HDI Due to Inequality" int   NOT NULL,
-    "Inequality-Adjusted Education Index" int   NOT NULL,
-    "Inequality-Adjusted Income Index" int   NOT NULL,
-    "Inequality-Adjusted Life Expectancy Index" int   NOT NULL,
-    "Income Inequality, Gini Coefficient" int   NOT NULL,
-    "Inequality in Education" int   NOT NULL,
-    "Inequality in Income" int   NOT NULL,
-    "Inequality in Life Expectancy" int   NOT NULL
-);
+CREATE TABLE [Life Expectancy Index] (
+    [Life expectancy at Birth] int  NOT NULL 
+)
 
-ALTER TABLE "World Happines Report" ADD CONSTRAINT "fk_World Happines Report_GDP per Capita_Life Ladder_Life Expectancy at Birth" FOREIGN KEY("GDP per Capita", "Life Ladder", "Life Expectancy at Birth")
-REFERENCES "Human Development Index HDI" ("GNI Index", "Education Index", "Life Expectancy Index");
+CREATE TABLE [Gross National Income] (
+    [GDP per Capita] int  NOT NULL ,
+    [GDP Total] int  NOT NULL ,
+    [GNI per Capita] int  NOT NULL 
+)
 
-ALTER TABLE "Education Index" ADD CONSTRAINT "fk_Education Index_Expected Years of Schooling" FOREIGN KEY("Expected Years of Schooling")
-REFERENCES "Human Development Index HDI" ("Education Index");
+ALTER TABLE [World Happines Report] WITH CHECK ADD CONSTRAINT [FK_World Happines Report_GDP per Capita] FOREIGN KEY([GDP per Capita])
+REFERENCES [Gross National Income] ([GNI per Capita])
 
-ALTER TABLE "Education Index" ADD CONSTRAINT "fk_Education Index_Mean Years of Schooling" FOREIGN KEY("Mean Years of Schooling")
-REFERENCES "Human Development Index HDI" ("Education Index");
+ALTER TABLE [World Happines Report] CHECK CONSTRAINT [FK_World Happines Report_GDP per Capita]
 
-ALTER TABLE "Life Expectancy Index" ADD CONSTRAINT "fk_Life Expectancy Index_Life expectancy at Birth" FOREIGN KEY("Life expectancy at Birth")
-REFERENCES "Human Development Index HDI" ("Life Expectancy Index");
+ALTER TABLE [World Happines Report] WITH CHECK ADD CONSTRAINT [FK_World Happines Report_Life Expectancy at Birth] FOREIGN KEY([Life Expectancy at Birth])
+REFERENCES [Life Expectancy Index] ([Life expectancy at Birth])
 
-ALTER TABLE "Gross National Income" ADD CONSTRAINT "fk_Gross National Income_GNI per Capita" FOREIGN KEY("GNI per Capita")
-REFERENCES "Human Development Index HDI" ("GNI Index");
+ALTER TABLE [World Happines Report] CHECK CONSTRAINT [FK_World Happines Report_Life Expectancy at Birth]
 
-ALTER TABLE "Inequality-Adjusted HDI" ADD CONSTRAINT "fk_Inequality-Adjusted HDI_Inequality-Adjusted Education Index_Inequality-Adjusted Income Index_Inequality-Adjusted Life Expectancy Index" FOREIGN KEY("Inequality-Adjusted Education Index", "Inequality-Adjusted Income Index", "Inequality-Adjusted Life Expectancy Index")
-REFERENCES "Human Development Index HDI" ("Education Index", "GNI Index", "Life Expectancy Index");
+ALTER TABLE [Human Development Index HDI] WITH CHECK ADD CONSTRAINT [FK_Human Development Index HDI_Education Index_Education Index] FOREIGN KEY([Education Index], [Education Index])
+REFERENCES [Education Index] ([Expected Years of Schooling], [Mean Years of Schooling])
 
-ALTER TABLE "Inequality-Adjusted HDI" ADD CONSTRAINT "fk_Inequality-Adjusted HDI_Inequality in Education_Inequality in Income_Inequality in Life Expectancy" FOREIGN KEY("Inequality in Education", "Inequality in Income", "Inequality in Life Expectancy")
-REFERENCES "Human Development Index HDI" ("Education Index", "GNI Index", "Life Expectancy Index");
+ALTER TABLE [Human Development Index HDI] CHECK CONSTRAINT [FK_Human Development Index HDI_Education Index_Education Index]
 
+ALTER TABLE [Human Development Index HDI] WITH CHECK ADD CONSTRAINT [FK_Human Development Index HDI_GNI Index] FOREIGN KEY([GNI Index])
+REFERENCES [Gross National Income] ([GNI per Capita])
+
+ALTER TABLE [Human Development Index HDI] CHECK CONSTRAINT [FK_Human Development Index HDI_GNI Index]
+
+ALTER TABLE [Human Development Index HDI] WITH CHECK ADD CONSTRAINT [FK_Human Development Index HDI_Life Expectancy Index] FOREIGN KEY([Life Expectancy Index])
+REFERENCES [Life Expectancy Index] ([Life expectancy at Birth])
+
+ALTER TABLE [Human Development Index HDI] CHECK CONSTRAINT [FK_Human Development Index HDI_Life Expectancy Index]
+
+COMMIT TRANSACTION QUICKDBD
